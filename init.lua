619a5b6071c4ba0b02c1d162e62b95e71457d260
Christopher Lee <akachrislee@gmail.com>
2016-11-29 16:07:17 -0500

    add any complete

--- init.lua
+++ init.lua
@@ -54,3 +54,39 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Down", function()
   win:setFrame(f)
 end)
 
+-- ANY COMPLETE BEGIN --
+local urlencode = require("urlencode")
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "G", function()
+    local GOOGLE_ENDPOINT = 'https://www.google.com/complete/search?client=hp&hl=en&xhr=t&q=%s'
+    local current = hs.application.frontmostApplication()
+
+    local chooser = hs.chooser.new(function(choosen)
+        current:activate()
+        hs.eventtap.keyStrokes(choosen.text)
+    end)
+
+    chooser:queryChangedCallback(function(string)
+        local query = urlencode.string(string)
+
+        hs.http.asyncGet(string.format(GOOGLE_ENDPOINT, query), nil, function(status, data)
+            if not data then return end
+
+            local results = hs.json.decode(data)
+
+            if not results then return end
+
+            choices = hs.fnutils.imap(results[2], function(result)
+                return {
+                    ["text"] = string.gsub(result[1], '</?b>', ''),
+                }
+            end)
+
+            chooser:choices(choices)
+        end)
+    end)
+
+    chooser:searchSubText(false)
+
+    chooser:show()
+end)
+-- ANY COMPLETE END --
